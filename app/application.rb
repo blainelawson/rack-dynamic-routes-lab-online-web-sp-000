018b3782aca10ab544c1e47393f1a714b06f9862
Blaine Lawson <blaine.lawson@gmail.com>
2019-05-03 16:32:48 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -8,6 +8,10 @@ class Application
 # binding.pry
     if req.path.match(/items/)
       binding.pry
+      item_name = req.path.split("/items/").last
+      item = Item.all.find{|i| i == item_name}
+
+      resp.write item
     else
       resp.write "400 Item not found"
     end

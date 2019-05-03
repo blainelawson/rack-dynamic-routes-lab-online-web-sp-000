cade8c1308b445ba546b2ee5c2957d745080efb9
Blaine Lawson <blaine.lawson@gmail.com>
2019-05-03 16:19:58 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,3 +3,10 @@ class Application
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
+
+    if req.path.match(/items/)
+    else
+      resp.write "400 Item does not exist"
+    end
+  end
+end

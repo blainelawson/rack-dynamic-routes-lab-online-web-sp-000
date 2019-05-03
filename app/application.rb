40f9ad68b26115aac080a14fed4358bcef8a1ba3
Blaine Lawson <blaine.lawson@gmail.com>
2019-05-03 16:25:44 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -5,7 +5,7 @@ class Application
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
-binding.pry
+# binding.pry
     if req.path.match(/items/)
     else
       resp.write "400 Item not found"
